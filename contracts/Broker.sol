08a032506b6bfb1b27a9bdf9c594358b9baa34fc
Hossein <kalex.hsn@gmail.com>
2023-09-18 19:53:25 +0330

    add orderbook to broker

--- contracts/Broker.sol
+++ contracts/Broker.sol
@@ -5,17 +5,15 @@ pragma solidity ^0.8.0;
 import {IPermit2} from "./interface/IPermit2.sol";
 import {IERC20} from "./interface/IERC20.sol";
 import {SafeMath} from "./library/SafeMath.sol";
+import {OrderBook} from "./OrderBook.sol";
 
-contract Broker {
-
+contract Broker is OrderBook {
     /// @notice permit2 address used for token transfers and signature verification
     IPermit2 public immutable permit2;
 
     constructor(IPermit2 _permit2) {
         permit2 = _permit2;
     }
-
-
 
     struct MatchedOrders {
         uint256 matchID;
@@ -33,15 +31,6 @@ contract Broker {
         uint256 takerNonce;
         bytes makerSignature;
         bytes takerSignature;
-        address optionContractAddress;
-    }
-
-    struct Order {
-        uint256 orderID;
-        uint64 deadline;
-        uint256 nonce;
-        uint256 permiumRatio;
-        uint256 optionAmount;
         address optionContractAddress;
     }
 }
---
+++ contracts/OrderBook.sol
@@ -0,0 +1,75 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract OrderBook {
+    mapping(uint256 => bytes32) public orderBook;
+
+    struct Order {
+        uint256 orderID;
+        bool isMaker;
+        uint256 optionAmount;
+        uint256 permiumRatio;
+        uint64 deadline;
+        uint256 nonce;
+        bytes signature;
+        address optionContractAddress;
+    }
+
+    event orderAdded(address owner, Order order);
+    event orderUpdated(address owner, Order newOrder);
+    event orderDeleted(address owner, Order deletedOrder);
+
+    function addOrder(Order memory order) external {
+        require(
+            uint256(orderBook[order.orderID]) == 0,
+            "ERROR : orderID already exist"
+        );
+        orderBook[order.orderID] = hash(msg.sender, order);
+
+        emit orderAdded(msg.sender, order);
+    }
+
+    function updateOrder(
+        Order memory oldOrder,
+        Order memory newOrder
+    ) external {
+        require(
+            orderBook[oldOrder.orderID] == hash(msg.sender, oldOrder),
+            "ERROR : order does not exist"
+        );
+        orderBook[oldOrder.orderID] = hash(msg.sender, newOrder);
+
+        emit orderUpdated(msg.sender, newOrder);
+    }
+
+    function deleteOrder(Order memory order) external {
+        require(
+            orderBook[order.orderID] == hash(msg.sender, order),
+            "ERROR : order does not exist"
+        );
+        orderBook[order.orderID] = bytes32(0);
+
+        emit orderDeleted(msg.sender, order);
+    }
+
+    function hash(
+        address owner,
+        Order memory order
+    ) public pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encode(
+                    owner,
+                    order.orderID,
+                    order.isMaker,
+                    order.optionAmount,
+                    order.permiumRatio,
+                    order.deadline,
+                    order.nonce,
+                    order.signature,
+                    order.optionContractAddress
+                )
+            );
+    }
+}
