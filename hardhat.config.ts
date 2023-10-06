a812c323d717792b5ad230d9a89dca334baf9507
Hossein <kalex.hsn@gmail.com>
2023-10-06 13:27:25 +0330

    add tracer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 const { privateKey, mumbaiApiKey } = require("./config.json")
 require("hardhat-contract-sizer");
+require("hardhat-tracer");
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
@@ -12,8 +13,9 @@ export default config;
 
 module.exports = {
   solidity: {
-    version: "0.8.19",
+    version: "0.8.17",
     settings: {
+      viaIR: true,
       optimizer: {
         enabled: true,
         runs: 1000,
@@ -25,8 +27,30 @@ module.exports = {
     polygon_mumbai: {
       url: mumbaiApiKey,
       accounts: [privateKey],
-    }
+      chainId : 80001
+    },
+    hardhat: {
+      allowUnlimitedContractSize: false,
+    },
+    // arbitrumRinkeby: {
+    //   url: `https://arbitrum-rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // arbitrum: {
+    //   url: `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // optimismKovan: {
+    //   url: `https://optimism-kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // optimism: {
+    //   url: `https://optimism-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // mumbai: {
+    //   url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
+    // polygon: {
+    //   url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // },
   },
 
-  defaultNetwork: "polygon_mumbai",
+  // defaultNetwork: "polygon_mumbai",
 };
